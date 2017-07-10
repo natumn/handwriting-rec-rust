ac39bcb52102fc36faa1981b1186559aee5e7302
koki natsume <natsume.pcuse75@gmail.com>
2017-07-10 15:33:15 +0900

    [Add]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate ndarray;
+use ndarray::prelude::*;
+
+fn get_data() -> (x_test: ,t_test:  ) {
+    let x_train,t_train,x_test,t_test = load_mnist(true, true, false);
+}
+
+//TODO: Reading weight parameter saved sample_weight.pkl
+fn init_network() -> {
+
+}
+
+//TODO: network build
+fn predict(network: Vec<f64>, x: Vec<f64>) -> f64 {
+    let w1 = Array::
+
+    let a1 = 
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x, t = get_data();
+    let mut network = init_network();
+    let accuracy_cnt = 0;
+
+
 }
--- src/mnist.rs
+++ src/mnist.rs
@@ -4,10 +4,13 @@ use std::path::path;
 use std::fs::File;
 
 struct Key_files {
-  train_img: String;
-  train_label: String;
-  test_img: String;
-  test_label: String;
+    train_img: String;
+    train_label: String;
+    test_img: String;
+    test_label: String;
+    dataset_dir: String;
+    save_file: ; String;
+  
 }
 
 trait MNIST {
@@ -15,46 +18,27 @@ trait MNIST {
 }
 
 impl MNIST for Key_files {
+    //TODO: mnist
   fn load_mnist(&self, normalize: bool, flatten: bool, one_hot_lobel: bool) -> Vec<String> {
+    let dataset_dir = 
     let mut dataset = serde_pickle::value_from_reader(dataset_dir+"/mnist.pkl");
-    if(nomalize) {
+    if nomalize  {
       for key in ("train_img", "test_img") {
         dataset[key] = dataset[key] as f64;
 	dataset[key] /= 255.0;
       }
     }
 
-    if(one_hot_label) {
+    if one_hot_label {
       dataset["train_label"] = change_hot_label(dataset["train_label"]);
       dataset["test_label"] = change_hot_label(dataset["test_label"]);
     }
 
-    if(!faltten) {
+    if !faltten  {
       for key in ("train_img, test_img") {
         //reshape(-1,1,28,28)
         //dataset[key] = dataset[key];
       }
     }
   }
-}
-
-//TODO: pythonで取得していたpickleファイルからデータ（学習済み）を取得。
-
-let url_base = "http://yann.lecun.com/exdb/mnist/";
-let key_files = Key_files {
-                  train_img: "train-images-idx3-ubyte.gz",
-		  train_label: "train-labels-idx1-ubyte.gz",
-		  test_img: "t10k-images-idx3-ubyte.gz",
-		  test_label: "t10k-labels-idx1-ubyte.gz"
-                };
-//絶対パスの取得
-let dataset_dir = "";
-let save_file = dataset_dir + "/mnist.pkl";
-
-//fn download(file_name: String) {
-//  let file_path = dataset_dir + "/" + file_name;
-//}
-
-pub fn load_minst(normalize: bool,flatten: bool,one_hot_label: bool){
-  let mnist_file = serde-pickle::de::value_form_reader();
 }
