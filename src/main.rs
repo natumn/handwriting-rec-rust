b8fd0d8c22591d3e956bc04686c4ed0f358b67dc
koki natsume <natsume.pcuse75@gmail.com>
2017-07-11 17:05:35 +0900

    [Add]

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,49 @@
-extern crate ndarray;
+extern crate serde_pickle;
+extern crate ndarray::linalg;
 use ndarray::prelude::*;
 
-fn get_data() -> (x_test: ,t_test:  ) {
+
+fn get_data() -> (x_test: Vec<f64>,t_test:  Vec<f64>) {
     let x_train,t_train,x_test,t_test = load_mnist(true, true, false);
 }
 
 //TODO: Reading weight parameter saved sample_weight.pkl
-fn init_network() -> {
-
+fn init_network() -> Vec<f64> {
+    let network = pickle::value_from_reader("/sample_weight.pkl");
 }
 
 //TODO: network build
-fn predict(network: Vec<f64>, x: Vec<f64>) -> f64 {
-    let w1 = Array::
+fn predict(network: Vec<f64>, x: Vec<f64>) -> Vec<f64> {
+    //network array has three weight (index: 0..2) and three bias (index: 3..5)
 
-    let a1 = 
+    let a1 = linalg::Dot::dot(x, network[0]) + network[3];
+    let z1 = sigmoid(a1);
+    let a2 = linalg::Dot::dot(z1, network[1]) + network[4];
+    let z2 = sigmoid(a2);
+    let a3 = linalg::Dot::dot(z2, network[2]) + network[5];
+    let y = softmax(a3);
+}
+
+
+fn softmax(x: f64) -> f64 {
+    
+}
+
+fn sigmoid(x: f64) -> f64 {
+    let e = 2.718;
+    1/(1 + e.pow(-x))
 }
 
 fn main() {
     let x, t = get_data();
     let mut network = init_network();
-    let accuracy_cnt = 0;
+    let mut accuracy_cnt = 0;
 
-
+    for (i,x) in x.enumerate() {
+        let p = predict(network, x).fold(0.0/0.0, f64::max);
+        if p == t[i] {
+            accracy_cut += 1;
+        }
+    }
+    println!("Accuracy: {}" accuracy_cut/x.len());
 }
--- src/mnist.rs
+++ src/mnist.rs
@@ -1,16 +1,13 @@
 extern crate serde;
 extern crate serde_pickle;
-use std::path::path;
+use std::path::Path;
 use std::fs::File;
 
 struct Key_files {
-    train_img: String;
-    train_label: String;
-    test_img: String;
-    test_label: String;
-    dataset_dir: String;
-    save_file: ; String;
-  
+    train_img: String,
+    train_label: String,
+    test_img: String,
+    test_label: String,
 }
 
 trait MNIST {
@@ -19,13 +16,15 @@ trait MNIST {
 
 impl MNIST for Key_files {
     //TODO: mnist
-  fn load_mnist(&self, normalize: bool, flatten: bool, one_hot_lobel: bool) -> Vec<String> {
-    let dataset_dir = 
-    let mut dataset = serde_pickle::value_from_reader(dataset_dir+"/mnist.pkl");
+  fn load_mnist(&self, normalize: bool, flatten: bool, one_hot_lobel: bool) -> Key_file {
+    
+    // try!(Path::new("/mnist.pkl").exsits());
+    let file_path = Path::new("mnist.pkl").exsits().expect("pkl_file not exist. ");  
+    let mut dataset = serde_pickle::value_from_reader(file_path);
     if nomalize  {
       for key in ("train_img", "test_img") {
         dataset[key] = dataset[key] as f64;
-	dataset[key] /= 255.0;
+	    dataset[key] /= 255.0;
       }
     }
 
